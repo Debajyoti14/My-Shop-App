ada72b64e4d2507a851c744a4286675044b04582
Debajyoti <debajyotisaha14@gmail.com>
2022-04-16 20:23:37 +0530

    Toggle Fav Button Done

--- lib/screens/product_overview.dart
+++ lib/screens/product_overview.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/widgets/product_item.dart';
-
-import '../models/product.dart';
 import '../widgets/product_grid.dart';
 
 class ProductOverview extends StatelessWidget {
