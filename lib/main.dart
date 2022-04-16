be470d0bb7f43ea049c600033324c804766a2ac5
Debajyoti <debajyotisaha14@gmail.com>
2022-04-16 16:22:33 +0530

    Provider functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import '/screens/product_detail_screen.dart';
 import '/screens/product_overview.dart';
+import './providers/product_providers.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverview(),
+        routes: {ProductDetail.routeName: (ctx) => ProductDetail()},
       ),
-      home: ProductOverview(),
-      routes: {ProductDetail.routeName: (ctx) => ProductDetail()},
     );
   }
 }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product_providers.dart';
+import '/widgets/product_item.dart';
+
+import '../models/product.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) => Productitem(
+        id: products[index].id,
+        imageUrl: products[index].imageUrl,
+        title: products[index].title,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
