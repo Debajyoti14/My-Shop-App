288ae5921b3f9b0d36c66764adf3f546eaf5ae73
Debajyoti <debajyotisaha14@gmail.com>
2022-04-17 11:14:02 +0530

    Order Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/cart_screen.dart';
-import '/providers/cart_item.dart';
 import 'package:provider/provider.dart';
 
+import '/providers/orders.dart';
+import '/providers/cart_item.dart';
+import './providers/product_providers.dart';
+
+import '/screens/cart_screen.dart';
 import '/screens/product_detail_screen.dart';
 import '/screens/product_overview.dart';
-import './providers/product_providers.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +21,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
         ),
       ],
       child: MaterialApp(
